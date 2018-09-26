9589c57615db9068a5315f1805fae04ba60863dc
Leo <leo.kwok@beamly.com>
2018-09-26 13:48:22 +0100

    6. Adding A Context Reducer For Actions

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,15 +3,6 @@ import Contact from './Contact';
 import { Consumer } from '../context';
 
  class Contacts extends Component {
-    deleteContact = id => {
-        const { contacts} = this.state;
-
-        const newContacts = contacts.filter(contact => contact.id !== id);
-
-        this.setState({
-            contacts: newContacts
-        });
-    };
 
     render() {
         return (
@@ -24,7 +15,6 @@ import { Consumer } from '../context';
                                 <Contact
                                     key={contact.id}
                                     contact={contact}
-                                    deleteClickHandler={this.deleteContact.bind(this, contact.id)}
                                 />
                             ))}
                         </React.Fragment>
