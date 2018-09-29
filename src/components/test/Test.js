653d421ba44db3cf44df39b302cb79e8be9a4fbe
Leo <leo.kwok@beamly.com>
2018-09-29 08:20:57 +0100

    2. GET Requests

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,44 +2,53 @@ import React, { Component } from 'react';
 
 class Test extends Component {
     state = {
-        test: 'test'
+        title: '',
+        id: ''
     };
 
     componentDidMount() {
-        console.log('componentDidMount...');
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(data => this.setState({
+                title: data.title,
+                body: data.body
+            })
+        );
     }
 
-    componentWillMount() {
-        console.log('componentWilldMount...');
-    }
+    // componentWillMount() {
+    //     console.log('componentWilldMount...');
+    // }
 
-    componentDidUpdate() {
-        console.log('componentDidUpdate...');
-    }
+    // componentDidUpdate() {
+    //     console.log('componentDidUpdate...');
+    // }
 
-    componentWillUpdate() {
-        console.log('componentWillUpdate...');
-    }
+    // componentWillUpdate() {
+    //     console.log('componentWillUpdate...');
+    // }
 
-    // usually used in redux
-    componentWillReceiveProps(nextProps, nextState) {
-        console.log('componentWillReceiveProps...');
-    }
+    // // usually used in redux
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     console.log('componentWillReceiveProps...');
+    // }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        return {
-            test: 'something'
-        };
-    }
+    // static getDerivedStateFromProps(nextProps, prevState) {
+    //     return {
+    //         test: 'something'
+    //     };
+    // }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log(' getSnapshotBeforeUpdate...');
-    }
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log(' getSnapshotBeforeUpdate...');
+    // }
 
   render() {
+      const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     )
   }
