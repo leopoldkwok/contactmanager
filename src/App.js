b611fd6f67ce019c12cccb7ec145ea1ceb01ff8c
Leo <leo.kwok@beamly.com>
2018-09-29 08:06:52 +0100

    1. Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
+import Test from './components/test/Test';
 
 
 import { Provider } from './context';
@@ -24,6 +25,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/About" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
---
+++ src/components/test/Test.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    state = {
+        test: 'test'
+    };
+
+    componentDidMount() {
+        console.log('componentDidMount...');
+    }
+
+    componentWillMount() {
+        console.log('componentWilldMount...');
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate...');
+    }
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate...');
+    }
+
+    // usually used in redux
+    componentWillReceiveProps(nextProps, nextState) {
+        console.log('componentWillReceiveProps...');
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+            test: 'something'
+        };
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log(' getSnapshotBeforeUpdate...');
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Test;
