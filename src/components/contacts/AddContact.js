124ae45f8b2c375e2eee5662a59c9a73d98960ac
Leo <leo.kwok@beamly.com>
2018-09-29 09:34:04 +0100

    4. Using AsyncAwait

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -11,7 +11,7 @@ class AddContact extends Component {
         errors: {}
     };
 
-    onSubmit = (dispatch, e) => {
+    onSubmit = async (dispatch, e) => {
         e.preventDefault();
         
         const { name, email, phone } = this.state;
@@ -37,8 +37,9 @@ class AddContact extends Component {
             errors: {}
         };
 
-        axios.post('https://jsonplaceholder.typicode.com/users', newContact)
-            .then(res => dispatch({type: 'ADD_CONTACT', payload: res.data}));
+        const res = await axios
+            .post('https://jsonplaceholder.typicode.com/users', newContact)
+            dispatch({type: 'ADD_CONTACT', payload: res.data});
 
         // Clear State
         this.setState({
