67914d57e1d15afe73da71c49cfaafe8739ec8fc
Leo <leo.kwok@beamly.com>
2018-09-30 20:34:54 +0100

    6. PUT Request & Update Action

--- src/context.js
+++ src/context.js
@@ -6,16 +6,25 @@ const Context = React.createContext();
 const reducer = (state, action) => {
     switch(action.type) {
         case 'DELETE_CONTACT': 
-        return {
-            ...state,
-            contacts: state.contacts.filter(contact => contact.id !== action.payload)
-        };
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            };
         case 'ADD_CONTACT': 
-        return {
-            ...state,
-            contacts: [action.payload, 
-            ...state.contacts]
-        };
+            return {
+                ...state,
+                contacts: [action.payload, 
+                ...state.contacts]
+            };
+        case 'UPDATE_CONTACT':
+            return {
+                ...state,
+                contacts: state.contacts.map(
+                    contact => contact.id === action.payload.id
+                        ? (contact = action.payload)
+                        : contact
+                )
+            };
         default: 
             return state;
     }
