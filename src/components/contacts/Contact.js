f4eec8979d647866001c7a5536413f8292d136ae
Leo <leo.kwok@beamly.com>
2018-09-30 10:26:07 +0100

    5. EditContact Component & Fetch Contact

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
 import axios from 'axios';
@@ -10,8 +11,7 @@ class Contact extends Component {
 
   onDeleteClick = async (id, dispatch) => {
     try {
-      await axios.delete
-      (`https://jsonplaceholder.typicode.com/users/${id}`);
+      await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
       dispatch({type: 'DELETE_CONTACT', payload: id});
     } catch(e) {
       dispatch({type: 'DELETE_CONTACT', payload: id});
@@ -41,8 +41,20 @@ class Contact extends Component {
                   className="fas fa-times" 
                   style={{cursor: 'pointer', float: 'right', color: 'red' }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
-
                 />
+
+                <Link to={`contact/edit/${id}`}>
+                  <i 
+                    className="fas fa-pencil-alt"
+                    style={{
+                        cursor: 'pointer',
+                        float: 'right',
+                        color: 'black',
+                        marginRight: '1rem'
+                    }}
+                  />
+                </Link>
+
               </h4>
               {showContactInfo ? ( <ul className="list-group">
                   <li className="list-group-item">Email: {email}</li>
